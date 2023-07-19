1fa961ae33358d04f902e83f61364382588560e3
tamimWeb <tamimweb.bd@gmail.com>
2023-07-19 08:47:40 +0600

    switch case

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 
 
  // GPA Calculator
-$marks = 107;
+ $marks = 32;
 
-if($marks >= 80 && $marks <=100){
+/*if($marks >= 80 && $marks <=100){
    echo 'You got A+';
 }elseif($marks >=70 && $marks <=79){
    echo 'You got A';
@@ -33,6 +33,38 @@ elseif($marks >= 0 && $marks <=32){
    echo 'You got F';
 }else{
    echo 'Invalid Number';
+}*/
+
+
+//swirch case
+switch($marks){
+   case($marks >= 80 && $marks <=100):
+      echo "You got A+";
+      break;
+
+   case($marks >= 70 && $marks <=79):
+      echo "You got A";
+      break;
+   case($marks >= 60 && $marks <=69):
+      echo "You got A-";
+      break;
+
+   case($marks >= 50 && $marks <=59):
+      echo "You got B";
+      break;
+   case($marks >= 40 && $marks <=49):
+      echo "You got C";
+      break;
+   case($marks >= 33 && $marks <=39):
+      echo "You got D";
+      break;
+   case($marks >= 0 && $marks <=32):
+       echo "You got F";
+       break;
+   default:
+      echo "Invalid Number";
+   
+   
 }
 
 
